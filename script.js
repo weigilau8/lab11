770ffc5fcd3483167bf8250a6dd37d98775f9c9b
weigilau8 <weigilauron@gmail.com>
2024-07-29 11:00:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeText(){
+
+
+    const elements = document. getElementsByClassName ('text');
+    for (let i=0; i < elements.length; i++){
+    elements[i].innerText = 'Text has been modified';
+    }
+}
